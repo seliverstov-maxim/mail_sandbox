e9afb8bcf996fd8136a1daa87f3fc5af371b5f55
Seliverstov Maxim <maxim.web.developer@gmail.com>
2014-07-16 13:34:28 +0000

    Use message params builder.

--- test/integration/em_server_test.rb
+++ test/integration/em_server_test.rb
@@ -4,24 +4,16 @@ class EmServerTest < MiniTest::Unit::TestCase
   def setup
     @em_host = 'localhost'
     @em_port = 2525
+
+    @connection = nil
+    @message_params = Message.build_attributes(@em_host, @em_port)
   end
 
   def test_multiple_recipients
-    c = nil
     EM.run {
-      EM.start_server(@em_host, @em_port, MailSandbox::Server) { |conn| c = conn }
-
+      EM.start_server(@em_host, @em_port, MailSandbox::Server) { |conn| @connection = conn }
       EM::Timer.new(5) { EM.stop }
-
-      EM::Protocols::SmtpClient.send ({
-        host: @em_host,
-        port: @em_port,
-        domain: "example.com",
-        from: "me@example.com",
-        to: ["you@example.com", "we@example.com"],
-        header: {"Subject"=>"Email subject line", "Reply-to"=>"me@example.com"},
-        body: "Not much of interest here."
-      })
+      EM::Protocols::SmtpClient.send(@message_params)
     }
 
     assert_equal( c.message.recipients, ["<you@example.com>", "<we@example.com>"] )
